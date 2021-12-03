f710cdc04a1df79336c5facc238e99a07663c595
Fox <cosmo8072@gmail.com>
2021-12-03 17:05:07 +0800

    Update

--- 2021/3p1/7.js
+++ 2021/3p1/7.js
@@ -1,3 +1,6 @@
 //DrunkMoon:  218  Javascript
 i=`${require('fs').readFileSync('t')}`.split`
 `;l=i[0].length;g=Array(l).fill(0);o=[...g];i.map(l=>{[...l].map((c,i)=>o[i]+=+c)});o.map((_,x)=>g[x]=+(_<i.length/2));t=parseInt(g.join``,2);console.log(t*(~t&~-(2<<~-l)))
+
+
+
